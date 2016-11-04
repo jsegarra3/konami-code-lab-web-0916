3c622bc7d978e992bb1f89c5a4b0178bd3c4d54f
James Segarra III <JamesSegarra@gmail.com>
2016-11-04 13:53:35 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
 function init() {
-  // your code here
-}
+  var counter = 0;
+
+  document.body.addEventListener('keydown',function(e){
+
+  const key = parseInt(e.which || e.detail);
+
+  if (key === code[counter]) {
+    counter++;
+    if (counter === code.length - 1) {
+      alert("#FucKonami");
+      counter = 0;
+    }
+  } else {
+    counter = 0;
+  }
+
+});//end addEventListener
+
+};//end init
